c597d682386d5668572e4f4ea295414470361192
theurrbc <rebecca.theurer@ruhr-uni-bochum.de>
2025-02-11 08:47:09 +0100

    Language has been corrected (ingame -> Germen; Comments->english); Show.names has been changed to allcreatures ArrayList. This has te be corrected in the rest of the code aswell

--- src/Main.java
+++ src/Main.java
@@ -10,140 +10,145 @@ public class Main {
     static ArrayList<CreatureStep3> creatures3 = new ArrayList<CreatureStep3>();
     static ArrayList<CreatureStep4> creatures4 = new ArrayList<>();
     static ArrayList<Creature> allcreatures = new ArrayList<>();
-//Achtung! Ich könnte auch alle Kreaturen in eine Array List vom Typ Creature packen, weil die untertypen vererbt sind.
-    //Ich mache das hier nicht. weil ich dann alles umscheiben müsste.
+//Attention! II could put all creatures into one Array List of the type creature, because the other creatrues are child-classes.
+    //I have done that now, I will have to change a lot of code before I can remove ArrayLists1,2,3,4
 
     public static void main(String[] args) {
 
-        int i = 0;//Counter für neu Kreierte lvl1 Kreaturen in case 1
-        //Navigation:
-        Scanner navigator = new Scanner(System.in);
-        int choice = 0;
-        int counter1 = 0; //counter creaturen lvl1
+        int counter1 = 0; //counter creatures lvl1
         int counter2 = 0;
         int counter3 = 0;
         int counter4 = 0;
 
+        Scanner navigator = new Scanner(System.in); //navigation
+        int choice = 0;
+
+
+        //programmstart
         System.out.println("\n\n--------------------------------------------------------------\n\nHello and welcome to 4ElementBreed!\n");
 
         while (true) {
-            System.out.println("Menu:");
-            System.out.println("create Creature (1)");
-            System.out.println("List your creatures (2)");
-            System.out.println("Fight (3)");
-            System.out.println("Breed (4)");
-            System.out.println("Change Creature name (5)");
-            System.out.println("Quit (6)");
+            //Menu
+            System.out.println("MenÜ:");
+            System.out.println("Kreatur kreieren (1)");
+            System.out.println("Kreaturen auflisten (2)");
+            System.out.println("Kämpfen (3)");
+            System.out.println("Kreaturen züchten (4)");
+            System.out.println("Namen von Kreaturen ändern (5)");
+            System.out.println("Speichern und Beenden (6)");
             choice = Navigate.number(">");
 
             switch (choice) {
                 case 1://Create Creature
-                    //Basiskreatur wird mit zufälligen Werten erstellt.
+                    //creates basecreature with random values.
                     basecreatures.add(new Creature());
-                    basecreatures.get(i).Create();
-                    //Basiskreatur wird in Stufe1 überführt. Hierbei: alle Werte mal 0.5 (Weil stufe1 schwächer ist als die höheren)
-                    // und Bonus wird je nach Element addiert
-                    creatures1.add(new CreatureStep1(basecreatures.get(i).name, basecreatures.get(i).maxHealth, basecreatures.get(i).armor, basecreatures.get(i).attack, basecreatures.get(i).speed, basecreatures.get(i).type));
-                    allcreatures.add(creatures1.get(i));
-                    System.out.println("\nThis Creature has been created:\n");
-                    creatures1.get(i).ShowCreature();
-                    i++;
+                    basecreatures.get(counter1).Create();
+                    //Basecreature is transferred to lvl1. This divides all values by 3 (Because creatures lvl1 are weaker than higher level creatures)
+                    // this step also adds a bonus dependig on the element
+                    creatures1.add(new CreatureStep1(basecreatures.get(counter1).name, basecreatures.get(counter1).maxHealth, basecreatures.get(counter1).armor, basecreatures.get(counter1).attack, basecreatures.get(counter1).speed, basecreatures.get(counter1).type));
+                    allcreatures.add(creatures1.get(counter1)); //I soon want to do vereything over the allcreatures arraylist.
+
+                    System.out.println("\nFolgende Kreatur wurde kreiert:\n");
+                    creatures1.get(counter1).ShowCreature();
                     counter1++;
                     break;
 
-                case 2://List your Creatures
-                    //Zuerst werden nur die Namen je Stufe aufgelistet und auf Nachfrage Details zu allen Kreaturen
-                    System.out.println("Your Creatures:");
-                    Show.names(creatures1, creatures2, creatures3, creatures4);
+                    case 2://List your Creatures
+                        //Lists all creatures names by step. Lists details on demand
+                        System.out.println("Deine Kreaturen:");
+                        Show.names(allcreatures);
 
-                    System.out.println("Deteils? (y/n)");
-                    String input = navigator.next().toLowerCase();
-                    if (input.equals("y")) {
-                        Show.all(creatures1, creatures2, creatures3, creatures4);
-                    }
-                    break;
+                        System.out.println("Deteils anzeigen? (y/n)");
+                        String input = navigator.next().toLowerCase();
+                        if (input.equals("y")) {
+                            Show.all(creatures1, creatures2, creatures3, creatures4);
+                        }
+                        break;
 
-                case 3://Fight
-                    Show.names(creatures1, creatures2, creatures3, creatures4);
-                    System.out.print("Welche Kreaturen sollen kämpfen?\nKreatur 1:\n>");
-                    int cr1 = navigator.nextInt();
-                    System.out.print("Kreatur 2:\n>");
-                    int cr2 = navigator.nextInt();
-                    Creature.fight(allcreatures.get(cr1), allcreatures.get(cr2));
-                    break;
-
-                case 4://Breed
-                    System.out.println("Show all Creatures? (y/n)");
-                    input = navigator.next().toLowerCase();
-                    if (input.equals("y")) {
-                        Show.all(creatures1, creatures2, creatures3, creatures4);
-                    }
-
-                    //Da nur Kreaturen der gleichen Stufe mit einander gezüchtet werden können, wird hier danach gefragt
-                    choice = Navigate.number("Welche Stufe sollen die Monster haben, die du kreuzen möchtest?\n>");
-
-                    //Den folgenden Prozess würde ich gerne in eine Klasse Exportieren aber ich weiß nicht, wie. Ich hatte bei meinem letzten Versuch Probleme
-                    //damit, die richtigen Variablen hin und her zu geben.
-                    switch (choice) {
-                        case 1:
-                            System.out.println("Zwei Stufe 1 Kreaturen mit einander kreuzen");
-                            int c1 = Navigate.number("Choose Creature 1\n>");
-                            int c2 = Navigate.number("Choose Creature 2\n>");
-                            if (c1>creatures1.size()-1 || c2>creatures1.size()-1) {
-                                System.out.println("***You chose a number that does not exist. Please try again.***");
-                                break;
-                            }
-                            if(creatures1.get(c1).sex == creatures1.get(c2).sex){
-                                System.out.println("***You can not breed same sex creatures***");
-                            }
-                            else {
-                                creatures2.add(new CreatureStep2(null, 0, 0, 0, 0, null, null, null).Breed1(creatures1.get(c1), creatures1.get(c2)));
-                                System.out.println("This Creature has been created:\n");
-                                creatures2.getLast().ShowCreature();
-                                allcreatures.add(creatures2.get(creatures2.size() - 1));
-                                counter2++;
-                            }
+                        case 3://Fight
+                            Show.names(allcreatures);
+                            System.out.print("Welche Kreaturen sollen kämpfen?\nKreatur 1:\n>");
+                            int cr1 = navigator.nextInt();
+                            System.out.print("Kreatur 2:\n>");
+                            int cr2 = navigator.nextInt();
+                            Creature.fight(allcreatures.get(cr1), allcreatures.get(cr2));
                             break;
-                        case 2:
-                            System.out.println("Zwei Stufe 2 Kreaturen mit einander kreuzen");
-                            c1 = Navigate.number("Choose Creature 1\n>");
-                            c2 = Navigate.number("Choose Creature 2\n>");
-                            if (c1>creatures2.size()-1 || c2>creatures2.size()-1) {
-                                System.out.println("***You chose a number that does not exist. Please try again.***");
+
+                            case 4://Breed
+                                System.out.println("Alle Kreaturen anzeigen? (y/n)");
+                                input = navigator.next().toLowerCase();
+                                if (input.equals("y")) {
+                                    Show.all(creatures1, creatures2, creatures3, creatures4);
+                                }
+
+                                //only creatures of the same step can be bred.
+                                choice = Navigate.number("Welche Stufe sollen die Monster haben, die du kreuzen möchtest?\n>");
+
+                                //I would like to export the following into a method. Previously I had problems with passing the right variables along
+                                switch (choice) {
+                                    case 1:
+                                        System.out.println("Zwei Stufe 1 Kreaturen mit einander kreuzen");
+                                        int c1 = Navigate.number("Wähle Kreatur 1\n>");
+                                        int c2 = Navigate.number("Wähle Kreatur 2\n>");
+                                        if (c1>creatures1.size()-1 || c2>creatures1.size()-1) {
+                                            System.out.println("***Du hast eine Nummer ausgewählt, die nicht existiert. Bitte versuche es erneut.***");
+                                            break;
+                                        }
+                                        if (c1>creatures1.size()-1 || c2>creatures1.size()-1) {
+                                            System.out.println("***You chose a number that does not exist. Please try again.***");
+                                            break;
+                                        }
+                                        if(creatures1.get(c1).sex == creatures1.get(c2).sex){
+                                            System.out.println("***Du kannst keine Kreaturen des selben Geschlechtes mit einander kreuzen***");
+                                        }
+                                        else {
+                                            creatures2.add(new CreatureStep2(null, 0, 0, 0, 0, null, null, null).Breed1(creatures1.get(c1), creatures1.get(c2)));
+                                            System.out.println("Folgende Kreatur wurde kreiert:\n");
+                                            creatures2.getLast().ShowCreature();
+                                            allcreatures.add(creatures2.get(creatures2.size() - 1));
+                                            counter2++;
+                                        }
+                                        break;
+                                        case 2:
+                                            System.out.println("Zwei Stufe 2 Kreaturen mit einander kreuzen");
+                                            c1 = Navigate.number("Wähle Kreatur 1\n>");
+                                            c2 = Navigate.number("Wähle Kreatur 2\n>");
+                                            if (c1>creatures2.size()-1 || c2>creatures2.size()-1) {
+                                                System.out.println("***Du hast eine Nummer ausgewählt, die nicht existiert. Bitte versuche es erneut.***");
+                                                break;
+                                            }
+                                            if(creatures2.get(c1).sex == creatures2.get(c2).sex){
+                                                System.out.println("***Du kannst keine Kreaturen des selben Geschlechtes mit einander kreuzen***");
+                                            }
+                                            else {
+                                                creatures3.add(new CreatureStep3(null, 0, 0, 0, 0, null, null).Breed2(creatures2.get(c1), creatures2.get(c2)));
+                                                System.out.println("Folgende Kreatur wurde kreiert:\n");
+                                                creatures3.get(creatures3.size() - 1).ShowCreature();
+                                                allcreatures.add(creatures3.get(creatures3.size() - 1));
+                                                counter3++;
+                                            }
+                                            break;
+                                            case 3:
+                                                System.out.println("Zwei Stufe 3 Kreaturen mit einander kreuzen");
+                                                c1 = Navigate.number("Wähle Kreatur 1\n>");
+                                                c2 = Navigate.number("Wähle Kreatur 2\n>");
+                                                if (c1>creatures3.size()-1 || c2>creatures3.size()-1) {
+                                                    System.out.println("***Du hast eine Nummer ausgewählt, die nicht existiert. Bitte versuche es erneut.***");
+                                                    break;
+                                                }
+                                                if(creatures3.get(c1).sex == creatures3.get(c2).sex){
+                                                    System.out.println("***Du kannst keine Kreaturen des selben Geschlechtes mit einander kreuzen***");
+                                                }
+                                                else {
+                                                    creatures4.add(new CreatureStep4(null, 0, 0, 0, 0, null, null).Breed3(creatures3.get(c1), creatures3.get(c2)));
+                                                    System.out.println("Folgende Kreatur wurde kreiert:\n");
+                                                    creatures4.get(creatures4.size() - 1).ShowCreature();
+                                                    allcreatures.add(creatures4.get(creatures4.size() - 1));
+                                                    counter4++;
+                                                }
+                                                break;
+                                }
                                 break;
-                            }
-                            if(creatures2.get(c1).sex == creatures2.get(c2).sex){
-                                System.out.println("***You can not breed same sex creatures***");
-                            }
-                            else {
-                                creatures3.add(new CreatureStep3(null, 0, 0, 0, 0, null, null).Breed2(creatures2.get(c1), creatures2.get(c2)));
-                                System.out.println("This Creature has been created:\n");
-                                creatures3.get(creatures3.size() - 1).ShowCreature();
-                                allcreatures.add(creatures3.get(creatures3.size() - 1));
-                                counter3++;
-                            }
-                            break;
-                        case 3:
-                            System.out.println("Zwei Stufe 3 Kreaturen mit einander kreuzen");
-                            c1 = Navigate.number("Choose Creature 1\n>");
-                            c2 = Navigate.number("Choose Creature 2\n>");
-                            if (c1>creatures3.size()-1 || c2>creatures3.size()-1) {
-                                System.out.println("***You chose a number that does not exist. Please try again.***");
-                                break;
-                            }
-                            if(creatures3.get(c1).sex == creatures3.get(c2).sex){
-                                System.out.println("***You can not breed same sex creatures***");
-                            }
-                            else {
-                                creatures4.add(new CreatureStep4(null, 0, 0, 0, 0, null, null).Breed3(creatures3.get(c1), creatures3.get(c2)));
-                                System.out.println("This Creature has been created:\n");
-                                creatures4.get(creatures4.size() - 1).ShowCreature();
-                                allcreatures.add(creatures4.get(creatures4.size() - 1));
-                                counter4++;
-                            }
-                            break;
-                    }
-                    break;
 
                 case 5: //Change Name
 
@@ -152,7 +157,7 @@ public class Main {
                     int Stufe = Navigate.number("Welchen Namen möchtest du ändern?\nStufe\n>");
                     int creaturenum = Navigate.number("Nummer der Kreatur\n>");
                     System.out.print("Bitte gib einen Namen ein\n>");
-                    String name = text.nextLine();//Achtung! Nimmt keine Leerzeichen. Aber warum???
+                    String name = text.nextLine();
 
                     if (Stufe == 1){
                         creatures1.get(creaturenum).name = name;
@@ -174,7 +179,7 @@ public class Main {
                     SaveFile.write2(counter2,creatures2);
                     SaveFile.write3(counter3,creatures3);
                     SaveFile.write4(counter4,creatures4);
-                    System.out.println("Creaturen wurden gespeichert. Bis zum nächsten Mal!\n\n");
+                    System.out.println("Kreaturen wurden gespeichert. Bis zum nächsten Mal!\n\n");
                     System.exit(0);
             }
         }
--- src/Show.java
+++ src/Show.java
@@ -2,49 +2,58 @@ import java.util.ArrayList;
 
 public class Show {
     static void all(ArrayList<CreatureStep1> creatures1, ArrayList<CreatureStep2> creatures2, ArrayList<CreatureStep3> creatures3, ArrayList<CreatureStep4> creatures4) {
-        System.out.println("Creatures Stufe 1 (common):\n");
+        System.out.println("Kreaturen Stufe 1 (gewöhnlich):\n");
         for (int i = 0; i < creatures1.size(); i++) {
             System.out.print(i + ")");
             creatures1.get(i).ShowCreature();
         }
-        System.out.println("Creatures Stufe 2 (special):\n");
+        System.out.println("Kreaturen Stufe 2 (spezial):\n");
         for (int i = 0; i < creatures2.size(); i++) {
             System.out.print(i + ")");
             creatures2.get(i).ShowCreature();
         }
-        System.out.println("Creatures Stufe 3 (epic):\n");
+        System.out.println("Kreaturen Stufe 3 (episch):\n");
         for (int i = 0; i < creatures3.size(); i++) {
             System.out.print(i + ")");
             creatures3.get(i).ShowCreature();
         }
-        System.out.println("Creatures Stufe 4 (legendary):\n");
+        System.out.println("Kreaturen Stufe 4 (legendär):\n");
         for (int i = 0; i < creatures4.size(); i++) {
             System.out.print(i + ")");
             creatures3.get(i).ShowCreature();
         }
     }
 
-    static void names(ArrayList<CreatureStep1> creatures1, ArrayList<CreatureStep2> creatures2, ArrayList<CreatureStep3> creatures3, ArrayList<CreatureStep4> creatures4) {
+
+    static void names(ArrayList<Creature> creatures) {
         System.out.println("Stufe 1:");
-        int num = 0;
-        for (int i = 0; i < creatures1.size(); i++) {
-            System.out.println(num + ") " + creatures1.get(i).name);
-            num++;
+        int num = 0; //can be used to list all creatures with conscutive numbers (looks nices but indices are wrong to pick a creature)
+        for (int i = 0; i < creatures.size(); i++) {
+            if (creatures.get(i).step ==1) {
+                System.out.println(i + ") " + creatures.get(i).name);
+                num++;
+            }
         }
         System.out.println("Stufe 2:");
-        for (int i = 0; i < creatures2.size(); i++) {
-            System.out.println(num + ") " + creatures2.get(i).name);
-            num++;
+        for (int i = 0; i < creatures.size(); i++) {
+            if (creatures.get(i).step ==2) {
+                System.out.println(i + ") " + creatures.get(i).name);
+                num++;
+            }
         }
         System.out.println("Stufe 3:");
-        for (int i = 0; i < creatures3.size(); i++) {
-            System.out.println(num + ") " + creatures3.get(i).name);
-            num++;
+        for (int i = 0; i < creatures.size(); i++) {
+            if (creatures.get(i).step ==3) {
+                System.out.println(i + ") " + creatures.get(i).name);
+                num++;
+            }
         }
         System.out.println("Stufe 4:");
-        for (int i = 0; i < creatures4.size(); i++) {
-            System.out.println(num + ") " + creatures4.get(i).name);
-            num++;
+        for (int i = 0; i < creatures.size(); i++) {
+            if (creatures.get(i).step ==4) {
+                System.out.println(i + ") " + creatures.get(i).name);
+                num++;
+            }
         }
     }
 }
